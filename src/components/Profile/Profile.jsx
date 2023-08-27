ae3fa4dd37b781399d7893ed2ab8a8957e019d44
MassloveDen <denis132141@yandex.ru>
2023-08-27 17:29:30 +0300

    JS hard working hope

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import './Profile.css';
+import Header from '../Header/Header';
+import { useNavigate } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
+
+function Profile() {
+  const { values, valid, handleChange, error } = useForm();
+  const [edit, setEdit] = React.useState(false);
+  const navigate = useNavigate();
+
+  function submitForm(e) {
+    e.preventDefault();
+  }
+
+  function handleEdit() {
+    setEdit(!edit);
+  }
+
+  return (
+    <>
+      <Header />
+      <main className='profile'>
+        <section className='profile__section'>
+          <h1 className='profile__title'>Привет, Денис!</h1>
+          <form className='profile__form' onSubmit={submitForm} name='account'>
+            <fieldset className='profile__fieldset profile__fieldset_form'>
+              <label className='profile__name' htmlFor='name'>
+                Имя
+              </label>
+              <input
+                className={`profile__input ${
+                  error.name ? 'profile__user_error' : ''
+                }`}
+                name='name'
+                type='text'
+                minLength='2'
+                maxLength='40'
+                id='name'
+                required
+                placeholder='Денис'
+                onChange={handleChange}
+                value={values.name || ''}
+                disabled={edit ? false : true}
+              />
+            </fieldset>
+            <fieldset className='profile__fieldset profile__fieldset_form'>
+              <label className='profile__name' htmlFor='email'>
+                E-mail
+              </label>
+              <input
+                className={`profile__input ${
+                  error.name ? 'profile__user_error' : ''
+                }`}
+                name='email'
+                type='email'
+                minLength='6'
+                maxLength='40'
+                id='email'
+                required
+                placeholder='pochta@yandex.ru'
+                onChange={handleChange}
+                value={values.email || ''}
+                disabled={edit ? false : true}
+              />
+            </fieldset>
+            {/* ниже код для проверки вывода сообщения */}
+            {/* {!valid ? (
+            <>
+              {edit ? (
+                <span id='name-error' className='profile__error_active'>
+                  При обновлении профиля произошла ошибка.
+                </span>
+              ) : (
+                ''
+              )}
+            </>
+          ) : (
+            ''
+          )} */}
+            {edit && (
+              <button
+                className='profile__save-form'
+                type='submit'
+                disabled={valid ? false : true}
+              >
+                Сохранить
+              </button>
+            )}
+          </form>
+          {edit ? (
+            ''
+          ) : (
+            <>
+              <button
+                className='profile__edit-form'
+                type='button'
+                onClick={handleEdit}
+              >
+                Редактировать
+              </button>
+              <button
+                type='button'
+                className='profile__exit-form'
+                onClick={() => navigate('/')}
+              >
+                Выйти из аккаунта
+              </button>
+            </>
+          )}
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Profile;
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import './Register.css';
+import FormContainer from '../FormContainer/FormContainer';
+
+function Register() {
+  const [isActiveError, setIsActiveError] = React.useState(true);
+
+  console.log(setIsActiveError);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <main className='register'>
+      <FormContainer
+        title='Добро пожаловать!'
+        name='register'
+        onSubmit={handleSubmit}
+        buttonText='Зарегистрироваться'
+      >
+        <fieldset className='form__fieldset'>
+          <label className='form__label' htmlFor='name'>
+            Имя
+          </label>
+          <input
+            className='form__input focus'
+            name='name'
+            type='text'
+            minLength='2'
+            maxLength='40'
+            id='name'
+            required
+            placeholder='Денис'
+          />
+        </fieldset>
+        <fieldset className='form__fieldset'>
+          <label className='form__label' htmlFor='email'>
+            E-mail
+          </label>
+          <input
+            className='form__input focus'
+            name='email'
+            type='email'
+            minLength='2'
+            maxLength='40'
+            id='email'
+            required
+            placeholder='pochta@yandex.ru'
+          />
+        </fieldset>
+        <fieldset className='form__fieldset'>
+          <label className='form__label' htmlFor='password'>
+            Пароль
+          </label>
+          <input
+            className='form__input form__input_error'
+            name='password'
+            type='password'
+            minLength='6'
+            maxLength='100'
+            id='password'
+            required
+            placeholder='Пароль'
+          />
+          <span
+            id='name-error'
+            className={`form__error ${
+              isActiveError ? 'form__error_visible' : ''
+            } `}
+          >
+            Что-то пошло не так...
+          </span>
+        </fieldset>
+      </FormContainer>
+    </main>
+  );
+}
+export default Register;
