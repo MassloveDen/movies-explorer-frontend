12812665822cfc4b4231eae1d50e5ddacc97beda
MassloveDen <78202134+MassloveDen@users.noreply.github.com>
2023-09-11 20:40:25 +0300

    remove movie filter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -162,82 +162,84 @@ function App() {
       });
   };
 
-  return (
-    <div className='app'>
-      <CurrentUserContext.Provider value={currentUser}>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                <Header loggedIn={loggedIn} />
-                <Main />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path='/movies'
-            element={
-              <ProtectedRoute
-                element={Movies}
-                loggedIn={loggedIn}
-                savedCards={savedCards}
-                onCardSave={handleSaveCard}
-                cardList={cardList}
-                setCardList={setCardList}
-                handleDeleteCard={handleDeleteCard}
-                movieFilter={movieFilter}
-                setMovieFilter={setMovieFilter}
-                isCardsLoading={isCardsLoading}
-                setIsCardsLoading={setIsCardsLoading}
-              ></ProtectedRoute>
-            }
-          />
-          <Route
-            path='/saved-movies'
-            element={
-              <ProtectedRoute
-                element={SavedMovies}
-                loggedIn={loggedIn}
-                savedMovies={savedCards}
-                setSavedMovies={setSavedCards}
-                onCardDelete={handleDeleteCard}
-                movieFilter={movieFilter}
-                setCardList={setCardList}
-                setMovieFilter={setMovieFilter}
-                isCardsLoading={isCardsLoading}
-                setIsCardsLoading={setIsCardsLoading}
-              ></ProtectedRoute>
-            }
-          />
-          <Route
-            path='/signup'
-            element={
-              <Register onRegister={handleRegistration} loggedIn={loggedIn} />
-            }
-          />
-          <Route
-            path='/signin'
-            element={<Login onLogin={handleLogin} loggedIn={loggedIn} />}
-          />
-          <Route
-            path='/profile'
-            element={
-              <ProtectedRoute
-                element={Profile}
-                loggedIn={loggedIn}
-                updateUser={handleUpdateUser}
-                success={success}
-                logout={handleLogout}
-              />
-            }
-          />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </CurrentUserContext.Provider>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<CurrentUserContext.Provider value={currentUser}>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<>
+								<Header loggedIn={loggedIn} />
+								<Main />
+								<Footer />
+							</>
+						}
+					/>
+					<Route
+						path='/movies'
+						element={
+							<ProtectedRoute
+								element={Movies}
+								loggedIn={loggedIn}
+								savedCards={savedCards}
+								onCardSave={handleSaveCard}
+								cardList={cardList}
+								films={films}
+								setFilms={setFilms}
+								setCardList={setCardList}
+								handleDeleteCard={handleDeleteCard}
+								movieFilter={movieFilter}
+								setMovieFilter={setMovieFilter}
+								isCardsLoading={isCardsLoading}
+								setIsCardsLoading={setIsCardsLoading}
+							></ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/saved-movies'
+						element={
+							<ProtectedRoute
+								element={SavedMovies}
+								loggedIn={loggedIn}
+								savedMovies={savedCards}
+								setSavedMovies={setSavedCards}
+								onCardDelete={handleDeleteCard}
+								movieFilter={movieFilter}
+								setCardList={setCardList}
+								setMovieFilter={setMovieFilter}
+								isCardsLoading={isCardsLoading}
+								setIsCardsLoading={setIsCardsLoading}
+							></ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/signup'
+						element={
+							<Register onRegister={handleRegistration} loggedIn={loggedIn} />
+						}
+					/>
+					<Route
+						path='/signin'
+						element={<Login onLogin={handleLogin} loggedIn={loggedIn} />}
+					/>
+					<Route
+						path='/profile'
+						element={
+							<ProtectedRoute
+								element={Profile}
+								loggedIn={loggedIn}
+								updateUser={handleUpdateUser}
+								success={success}
+								logout={handleLogout}
+							/>
+						}
+					/>
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</CurrentUserContext.Provider>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -1,63 +1,43 @@
 import React, { useEffect } from "react"
 import "./MoviesCardList.css"
 import MoviesCard from "../MoviesCard/MoviesCard"
-import { MOVIE_SHORT } from "../../../utils/constants"
 import Preloader from "../../Preloader/Preloader"
 
 function MoviesCardList({
-  movies,
-  addMoviesButton,
-  onCardSave,
-  savedCards,
-  handleDeleteCard,
-  movieFilter,
-  isCardsLoading,
-  qtyOfMoviesCard,
+	movies,
+	addMoviesButton,
+	onCardSave,
+	savedCards,
+	handleDeleteCard,
+	isCardsLoading,
+	qtyOfMoviesCard,
 }) {
   useEffect(() => {
     qtyOfMoviesCard()
   }, [movies])
 
-  return (
-    <>
-      {isCardsLoading ? (
-        <Preloader />
-      ) : (
-        <section className='cardlist'>
-          <ul className='cardlist__list'>
-            {movieFilter
-              ? movies
-                  .filter((movie) => {
-                    return movie.duration < MOVIE_SHORT
-                  })
-                  .slice(0)
-                  .map((movie) => (
-                    <MoviesCard
-                      movie={movie}
-                      movies={movies}
-                      onCardSave={onCardSave}
-                      savedCards={savedCards}
-                      handleDeleteCard={handleDeleteCard}
-                      key={movie.id}
-                    />
-                  ))
-              : movies
-                  .slice(0, addMoviesButton)
-                  .map((movie) => (
-                    <MoviesCard
-                      movie={movie}
-                      movies={movies}
-                      onCardSave={onCardSave}
-                      savedCards={savedCards}
-                      handleDeleteCard={handleDeleteCard}
-                      key={movie.id}
-                    />
-                  ))}
-          </ul>
-        </section>
-      )}
-    </>
-  )
+	return (
+		<>
+			{isCardsLoading ? (
+				<Preloader />
+			) : (
+				<section className='cardlist'>
+					<ul className='cardlist__list'>
+						{movies.slice(0, addMoviesButton).map((film) => (
+							<MoviesCard
+								movie={film}
+								movies={movies}
+								onCardSave={onCardSave}
+								savedCards={savedCards}
+								handleDeleteCard={handleDeleteCard}
+								key={film.id}
+							/>
+						))}
+					</ul>
+				</section>
+			)}
+		</>
+	);
 }
 
 export default MoviesCardList
